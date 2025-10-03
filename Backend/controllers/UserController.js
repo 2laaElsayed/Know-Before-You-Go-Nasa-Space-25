12a278783a23ef0b9ccb80fe58e57f9fdadf5f91
TahanyEmad <tahanyemad30@gmail.com>
2025-10-03 23:47:26 +0300

    Create UserController.js

---
+++ Backend/controllers/UserController.js
@@ -0,0 +1,56 @@
+const Event = require("../models/event");
+const Favorite = require("../models/favorite");
+const User = require("../models/Users");
+const JSend = require("../utils/Jsend");
+const Constants = require("../utils/constant");
+const axios = require("axios");
+
+const ML_BASE_URL = process.env.ML_URL || "https://tahany.pythonanywhere.com";
+const provinceCoordinates = require("../utils/provinceCoordinates");
+
+
+const getThreeDayForecastForUser = async (req, res, next) => {
+  try {
+    const userId = req.user.id;
+
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(Constants.STATUSCODE.NOT_FOUND).json(
+        JSend.fail({ message: "User not found" })
+      );
+    }
+
+    let lat = user.lat;
+    let lon = user.lon;
+
+    if (lat == null || lon == null) {
+      const coords = provinceCoordinates[user.province];
+      if (!coords) {
+        return res.status(Constants.STATUSCODE.BAD_REQUEST).json(
+          JSend.fail({ message: "Province coordinates not found" })
+        );
+      }
+      lat = coords.lat;
+      lon = coords.lon;
+    }
+
+    const mlResp = await axios.post(`${ML_BASE_URL}/predict_next_days`, {
+      latitude: lat,
+      longitude: lon,
+      days: 3
+    });
+
+    return res.status(Constants.STATUSCODE.SUCCESS).json(
+      JSend.success({
+        userId: user._id,
+        username: user.username,
+        forecastNext3Days_mm: mlResp.data.predictedRain_mm
+      })
+    );
+  } catch (err) {
+    next(err);
+  }
+};
+module.exports = { 
+    getThreeDayForecastForUser 
+};

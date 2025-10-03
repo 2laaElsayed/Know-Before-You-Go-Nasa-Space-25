73308bef6843a27551027dfef9efb2e31a738326
TahanyEmad <tahanyemad30@gmail.com>
2025-10-03 11:32:25 +0300

    ADD Event Controllers and validations

--- Backend/app.js
+++ Backend/app.js
@@ -3,8 +3,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var ErrorHandler = require('./middelwares/ErrorHandler');
+var cors = require('cors');   
 
 var authRouter = require('./routes/auth');
+var eventRouter = require('./routes/event');
 
 var app = express();
 
@@ -13,6 +15,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+
+const allowedOrigins = [
+  "http://localhost:5173", 
+  "https://know-before-you-go.netlify.app"
+];
+
+app.use(cors({
+  origin: allowedOrigins,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
 /* Homepage route to check if server is running */
 app.get("/", (req, res) => {
   res.send("Server is running!");
@@ -20,6 +34,7 @@ app.get("/", (req, res) => {
 
 /* Routes */
 app.use('/api/auth', authRouter);
+app.use('/api/events', eventRouter);
 
 app.use(ErrorHandler);
 
